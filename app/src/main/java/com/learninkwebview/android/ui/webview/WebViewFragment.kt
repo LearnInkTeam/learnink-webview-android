c41e1ecc5a54c5753d72d1a3ffb1d09fd764e5fe
Georgia Barrie <georgiabarrie@gmail.com>
2023-09-08 16:51:58 +0100

    Updating webview loading

--- app/src/main/java/com/learninkwebview/android/ui/webview/WebViewFragment.kt
+++ app/src/main/java/com/learninkwebview/android/ui/webview/WebViewFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.WebResourceRequest
 import androidx.fragment.app.Fragment
 import com.learninkwebview.android.R
 import android.net.Uri
@@ -21,31 +22,40 @@ class WebViewFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_webview, container, false)
     }
 
+    fun handleUrlOverride(uri: Uri?): Boolean {
+        if (uri == null) {  // When the uri is malformed, let's override but not pass an intent (essentially doing nothing and ignoring the request)
+            return true
+        }
+        val internalHosts = listOf("m.learn.ink")
+
+        return if (internalHosts.contains(uri.host)) {  // Or some other applicable filtering strategy
+            false
+        } else {
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+            true
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val myWebView: WebView = view.findViewById(R.id.webView)
         myWebView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(
-                view: WebView,
-                url: String
+                view: WebView?,
+                request: WebResourceRequest?
             ): Boolean {
-                // Open Learn.ink urls inside the WebView
-                if (url.contains("m.learn.ink")) {
-                    view.loadUrl(url)
-                }
-                // Open all other urls in the browser
-                else {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    view.context.startActivity(intent)
-                }
-                return false
+                return handleUrlOverride(request?.url)
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean { // We override the deprecated function too for backwards compat
+                return handleUrlOverride(Uri.parse(url))
             }
         }
 
-        // Load the correct url
-        myWebView.loadUrl("https://m.learn.ink/acme/learning")
-        // Enable Javascript, content access and DOM storage
         myWebView.settings.javaScriptEnabled = true
         myWebView.settings.allowContentAccess = true
         myWebView.settings.domStorageEnabled = true
+        // Load the correct url
+        myWebView.loadUrl("https://m.learn.ink/acme/learning")
     }
 }
